4e55153a7204dba957370ae067968af07e755064
Hongkaibin <1341541192@qq.com>
2020-04-21 15:19:48 +0800

    项目基本结构

---
+++ spring-springmvc-hibernate/src/test/java/myTest.java
@@ -0,0 +1,29 @@
+import com.memory.dao.PersonDao;
+import com.memory.pojo.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:appliciationContext.xml")
+public class myTest {
+
+    @Resource(name = "personDao")
+    private PersonDao personDao;
+    @Test
+    public void testSave() {
+        Person p = new Person();
+        p.setUserName("Winbee");
+        p.setSex("女");
+        p.setPassword("123");
+        System.out.println(p);
+        personDao.save(p);
+
+    }
+}
